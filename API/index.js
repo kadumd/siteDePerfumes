4d7471657c27ec789018772565ef7d169d62e2b6
cadumd <cadu.md2021@gmail.com>
2025-02-15 21:23:26 -0300

    jhj

--- API/index.js
+++ API/index.js
@@ -156,6 +156,10 @@ const servidor = http.createServer((pedido, resposta) => {
         //==========================================================================================================================
         
         //logo=========
+        case '/imgs/logo.png':
+            resposta.writeHead(200, { "Content-Type": "image/png" });
+            resposta.end(fs.readFileSync('./frontend/imgs/logo.png'));
+            break
 
         case '/imgs/bg.png':
             resposta.writeHead(200, { "Content-Type": "image/png" });
